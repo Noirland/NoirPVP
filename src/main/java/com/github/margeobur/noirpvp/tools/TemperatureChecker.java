ab937e1b44e0e8339a910eb91ad2af3ef1d9221e
margeobur <margeobur@gmail.com>
2018-11-14 21:46:38 +1300

    moving to laptop

--- src/main/java/com/github/margeobur/noirpvp/tools/TemperatureChecker.java
+++ src/main/java/com/github/margeobur/noirpvp/tools/TemperatureChecker.java
@@ -1,4 +1,19 @@
 package com.github.margeobur.noirpvp.tools;
 
-public class TemperatureChecker {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Collection;
+
+/**
+ * A runnable that gets scheduled to run every X seconds. TemperatureChecker adjusts a player's hunger level
+ * based on the temperature of the area surrounding them.
+ */
+public class TemperatureChecker extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+    }
 }
